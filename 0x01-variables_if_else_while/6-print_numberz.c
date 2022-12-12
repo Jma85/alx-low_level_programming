82b2308b101cbcf966367e67ab5120e84d56a34d
jannat <jma24834@gmail.com>
2022-12-12 07:41:07 -0800

    Six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Description: main-program entry point.
+ * Return:0 no error, non zero if error.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
